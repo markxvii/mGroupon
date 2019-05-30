39260c5e224189bce1edf4e9302fc5326cf34329
Marc <markxvii@outlook.com>
2019-05-31 01:07:15 +0800

    Initial commit

---
+++ components/public/header/geo.vue
@@ -0,0 +1,17 @@
+<template>
+	<div class="m-geo">
+		<i class="el-icon-location"/>北京市
+		<nuxt-link class="changeCity" to="/changeCity">切换城市</nuxt-link>
+		[hongkong chengdu beijing]
+	</div>
+</template>
+
+<script>
+  export default {
+    name: 'geo'
+  }
+</script>
+
+<style>
+
+</style>
---
+++ layouts/default.vue
@@ -0,0 +1,25 @@
+<template>
+  <el-container class="layout-default">
+    <el-header>
+			<my-header></my-header>
+		</el-header>
+    <el-main>
+      <div>
+        <nuxt/>
+      </div>
+    </el-main>
+    <el-footer>Footer</el-footer>
+  </el-container>
+</template>
+
+<script>
+	import MyHeader from '@/components/public/header/index.vue'
+	export default {
+		components:{
+			MyHeader
+		}
+	}
+</script>
+
+<style>
+</style>
